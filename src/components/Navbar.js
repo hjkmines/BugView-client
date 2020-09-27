ce4ee257e6b7553d509cf098f2f1a44045572e5c
Hyung Kim <hjkmines@gmail.com>
2020-09-26 23:30:54 -0600

    adds refresh of local storeage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
-import React from 'react'; 
+import userEvent from '@testing-library/user-event';
+import React, { useContext } from 'react'; 
 import { Link } from 'react-router-dom'; 
+import { UserContext } from '../App'; 
 
 const Navbar = () => {
+
+    const { state, dispatch } = useContext(UserContext); 
+    const renderedList = () => {
+        if(state) {
+            return [
+                <li><Link to="/profile">My Profile</Link></li>, 
+                <li><Link to="/createpost">Create New Ticket</Link></li>
+            ]
+        } else {
+            return [
+                <li><Link to="/login">Log In</Link></li>,
+                <li><Link to="/signup">Register</Link></li>
+            ]
+        }
+    }
+
     return (
         <div>
             <nav>
                 <div class="nav-wrapper">
                     <Link to="/" class="brand-logo">BugView</Link>
                         <ul id="nav-mobile" class="right hide-on-med-and-down">
-                            <li><Link to="/login">Log In</Link></li>
-                            <li><Link to="/signup">Register</Link></li>
-                            <li><Link to="/profile">My Profile</Link></li>
-                            <li><Link to="/createpost">Create New Ticket</Link></li>
+                        {renderedList()}
                         </ul>
                 </div>
             </nav>
