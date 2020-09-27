c4cf6e4dda114859dd6450e2c2d77c36f04f1ade
Hyung Kim <hjkmines@gmail.com>
2020-09-26 21:20:21 -0600

    adds fetch for register

--- src/components/pages/CreatePost.js
+++ src/components/pages/CreatePost.js
@@ -2,13 +2,26 @@ import React from 'react';
 
 const CreatePost = () => {
     return (
-        <div className='card input-filled'>
+        <div 
+            className='card input-filled'
+            style={{
+                margin: '30px auto', 
+                maxWidth: '500px', 
+                padding: '20px', 
+                textAlign: 'center'
+            }}
+        >
             <input type='text' placeholder='Title' />
             <input type='text' placeholder='Description' />
             <input type='text' placeholder='Due Date' />
             <input type='text' placeholder='GitHub Link' />
             <input type='text' placeholder='Assign Team Members' />
             <input type='text' placeholder='Severity' />
+            <button 
+                className='btn waves-effect waves-light #64b5f6 blue lighten-2' 
+            >
+                Submit Ticket 
+            </button>
         </div>
     )
 }; 
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,7 +1,32 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import { Link } from 'react-router-dom'; 
 
 const Signup = () => {
+    const [firstName, setFirstName] = useState(''); 
+    const [lastName, setLasttName] = useState(''); 
+    const [jobTitle, setJobTitle] = useState(''); 
+    const [email, setEmail] = useState(''); 
+    const [password, setPassword] = useState(''); 
+
+    const PostData = () => {
+        fetch('/signup', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify({
+                firstName: '', 
+                lastName: '', 
+                jobTitle: '', 
+                email: '', 
+                password: ''
+            })
+        }).then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
     return (
         <div className='mycard'>
         <div className='card auth-card input field'>
@@ -9,25 +34,36 @@ const Signup = () => {
             <input 
                 type='text'
                 placeholder='First Name'
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
             />
             <input 
                 type='text'
                 placeholder='Last Name'
+                value={lastName}
+                onChange={(e) => setLasttName(e.target.value)}
             />
             <input 
                 type='text'
                 placeholder='Job Title'
+                value={jobTitle}
+                onChange={(e) => setJobTitle(e.target.value)}
             />
             <input 
                 type='text'
                 placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
             <input 
                 type='password'
                 placeholder='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
             <button 
                 className='btn waves-effect waves-light #64b5f6 blue lighten-2' 
+                onClick={() => PostData()}
             >
                 Register
             </button>
