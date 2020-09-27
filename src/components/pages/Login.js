a4bf309bd275e18bba0ee8122940e557f7de5c5d
Hyung Kim <hjkmines@gmail.com>
2020-09-26 21:46:35 -0600

    adds successful user login component

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,41 @@
-import React from 'react'; 
-import { Link } from 'react-router-dom'; 
+import React, { useState } from 'react'; 
+import { Link, useHistory } from 'react-router-dom'; 
+import M from 'materialize-css'; 
 
 const Login = () => {
+
+    const history = useHistory(); 
+    const [email, setEmail] = useState(''); 
+    const [password, setPassword] = useState(''); 
+
+    const PostData = () => {
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            M.toast({ html: 'Invalid email', classes: '#c62828 red darken-3' })
+            return 
+        }
+
+        fetch('/signin', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify({
+                email, 
+                password
+            })
+        }).then(res => res.json())
+        .then(data => {
+            if(data.error) {
+                console.log(data)
+                M.toast({ html: data.error, classes: '#c62828 red darken-3' })
+            } else {
+                M.toast({ html: 'Signed In Successfully', classes: '#43a047 green darken-1' })
+                history.push('/')
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }
 
     return (
         <div className='mycard'>
@@ -10,13 +44,18 @@ const Login = () => {
             <input 
                 type='text'
                 placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
             <input 
                 type='password'
-                placeholder='Password'
+                placeholder='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
             <button 
                 className='btn waves-effect waves-light #64b5f6 blue lighten-2' 
+                onClick={() => PostData()}
             >
                 Submit
             </button>
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -11,6 +11,11 @@ const Signup = () => {
     const [password, setPassword] = useState(''); 
 
     const PostData = () => {
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            M.toast({ html: 'Invalid email', classes: '#c62828 red darken-3' })
+            return 
+        }
+
         fetch('/signup', {
             method: 'POST', 
             headers: {
@@ -31,6 +36,8 @@ const Signup = () => {
                 M.toast({ html: data.message, classes: '#43a047 green darken-1' })
                 history.push('/login')
             }
+        }).catch(err => {
+            console.log(err)
         })
     }
 
