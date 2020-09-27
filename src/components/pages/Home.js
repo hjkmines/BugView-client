9c34893a937920aa424dee45ee0fb31fa8b28e73
Hyung Kim <hjkmines@gmail.com>
2020-09-27 12:36:26 -0600

    adds delete method

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -67,15 +67,41 @@ const Home = () => {
         })
     }
 
+    const deletePost = (postid) => {
+        fetch(`/deletepost/${postid}`, {
+            method: 'DELETE', 
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+            }
+        }).then(res => res.json())
+        .then(result => {
+            console.log(result)
+            const newData = data.filter(item => {
+                return item._id !== result._id 
+            })
+            setData(newData)
+        })
+    }
+
     return (
         <div className='home'>
             {
                 data.map( item => {
                     return (
                         <div className='card home-card' key={item._id}>
-                            <h5>{item.postedBy.firstName} {item.postedBy.lastName}</h5>
+                            <h5>{item.postedBy.firstName} {item.postedBy.lastName} {item.postedBy._id == state._id 
+                            && <i 
+                                className="material-icons" 
+                                style={{ float: 'right' }}
+                                onClick={() => deletePost(item._id)}
+                                >
+                                delete
+                            </i>
+                            
+                            }</h5>
                             <div className='card-image'>
                             <h5>{item.title}</h5> 
+                            
                             </div>
                             <div className='card-content'>
                             <i 
